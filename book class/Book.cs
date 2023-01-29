fce2b7ae5fef59be1338b23ed611c4fed6d41ae8
RizvanVeliyev <veliyevrizvan250@gmail.com>
2023-01-29 09:58:22 +0400

    Add project files.

---
+++ book class/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace book_class
+{
+	internal class Book
+	{
+		internal string name, Autname;
+		internal int PubDate, PageCount;
+
+		public override string ToString()
+		{
+			return $"{Autname} ''{name}'' {PubDate}";
+		}
+
+	}
+}
